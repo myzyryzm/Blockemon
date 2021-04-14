2e67d06ea9b1deb83303689116ff63f5ca56c8e9
myzyryzm <ryanyoung3e@yahoo.com>
2021-04-13 18:21:05 -0700

    adding

---
+++ Assets/Blockemon/Scripts/Dragon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Blockemon
+{
+    public class Dragon : MonoBehaviour
+    {
+        public Renderer bodyRenderer;
+    }
+}
---
+++ Assets/Blockemon/Scripts/ModifyTexture.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Blockemon 
+{
+    public class ModifyTexture : MonoBehaviour
+    {
+        public Dragon dragonPrefab;
+        Dragon dragon;
+
+        void Awake()
+        {
+            dragon = Instantiate(dragonPrefab.gameObject).GetComponent<Dragon>();
+            StartCoroutine(GetTexture());
+        }
+
+        IEnumerator GetTexture() {
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://ryzm-public.s3-us-west-1.amazonaws.com/T_Dragon.jpg");
+            yield return www.SendWebRequest();
+            if(www.isNetworkError == true)
+            {
+                Debug.Log(www.error);
+            }
+            else 
+            {
+                Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+                dragon.bodyRenderer.material.SetTexture("_MainTex", myTexture);
+            }
+    }
+    }
+}
+
